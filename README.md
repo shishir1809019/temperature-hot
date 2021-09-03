7896d72214e17d4eef8864753bbd405696d552a9
shishir1809019 <shishirdas726@gmail.com>
2021-09-03 16:22:06 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## temperature-hot
+
+### HTML || BOOTSTRAP || JS DOM || API CALL
+
+the main purpose of the project is call api --> load data --> show data in UI.
+
+### API(By city name)
+
+https://api.openweathermap.org/data/2.5/weather?q={city%20name}&appid={API%20key}
---
+++ index.js
@@ -0,0 +1,41 @@
+const spinner = document.getElementById("spinner");
+const loadCityWeather = () => {
+  const cityNameText = document.getElementById("input-field");
+  const cityName = cityNameText.value;
+  cityNameText.value = "";
+  document.getElementById("weather-container").textContent = "";
+  console.log(cityName);
+
+  if (cityName == "") {
+    //
+  } else {
+    spinner.removeAttribute("hidden");
+
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${"c4e705f458c9c6f55f91e0029eaea0eb"}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        spinner.setAttribute("hidden", "");
+        displayCityWeather(data);
+      });
+  }
+};
+
+const displayCityWeather = (data) => {
+  console.log(data);
+  const weatherContainer = document.getElementById("weather-container");
+  // weatherContainer.textContent = "";
+  const div = document.createElement("div");
+  div.innerHTML = `
+      <img src="https://openweathermap.org/img/wn/${
+        data.weather[0].icon
+      }.png" alt="" />
+          <h1 >${data.name}</h1>
+          <h3><span>${(Number(data.main.temp) - 273).toFixed(
+            2
+          )}</span>&deg;C</h3>
+          <h1 class="lead">${data.weather[0].main}</h1>
+      `;
+  weatherContainer.appendChild(div);
+};
